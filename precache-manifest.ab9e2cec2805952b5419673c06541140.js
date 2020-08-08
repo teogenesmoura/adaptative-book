dfca3bb5c0242f865840a57b93ad4d98d3680635
teogenesmoura <mourateogenes@gmail.com>
2020-08-08 11:18:17 -0300

    Updates

---
+++ precache-manifest.ab9e2cec2805952b5419673c06541140.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b7f6af9fb8a729ddd1fac4bc979c51",
+    "url": "/adaptative-book/index.html"
+  },
+  {
+    "revision": "582e4de58f4f419d1f22",
+    "url": "/adaptative-book/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "93715927faf335607de8",
+    "url": "/adaptative-book/static/js/2.014c7d83.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/adaptative-book/static/js/2.014c7d83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "582e4de58f4f419d1f22",
+    "url": "/adaptative-book/static/js/main.74925be8.chunk.js"
+  },
+  {
+    "revision": "90f7a62674dcfb0ddc3b",
+    "url": "/adaptative-book/static/js/runtime-main.6de30e62.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/adaptative-book/static/media/logo.5d5d9eef.svg"
+  }
+]);
